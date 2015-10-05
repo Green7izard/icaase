b89fc725c0ab668a5f7b238ae8826999af447c5a
Bas van Summeren <basvansummeren@hotmail.com>
2015-10-05 14:35:44 +0200

    Added InterestType Improved Intrest, its now a hibernate entity

--- spring-ws/Calculator/src/main/java/nl/dare2date/matching/interests/Interest.java
+++ spring-ws/Calculator/src/main/java/nl/dare2date/matching/interests/Interest.java
@@ -1,7 +1,59 @@
 package nl.dare2date.matching.interests;
 
+
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.*;
+
 /**
  * Created by Bas on 5-10-2015.
  */
+@DynamicUpdate
+@Table(appliesTo = "INTEREST")
 public class Interest {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private InterestType type;
+
+    public Interest(){}
+    public Interest(String name, InterestType type)
+    {
+        this.name=name;
+        this.type=type;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId( int id ) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public InterestType getType() {
+        return type;
+    }
+
+    public void setType(InterestType type) {
+        this.type = type;
+    }
 }
---
+++ spring-ws/Calculator/src/main/java/nl/dare2date/matching/interests/InterestType.java
@@ -0,0 +1,21 @@
+package nl.dare2date.matching.interests;
+
+/**
+ * Created by Bas on 5-10-2015.
+ */
+public enum InterestType {
+    CHANNEL,
+    MUSIC,
+    MOVIE,
+    TVSERIES,
+    BOOK,
+    SPORT,
+    GAME,
+    ANIMAL,
+    CREATIVE,
+    COLLECTION,
+    CULTURE,
+    TINKERING,
+    OTHER
+
+}
