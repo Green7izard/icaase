65bb9a2a8afbe52f452112a37ceab10d411c36d2
Bas van Summeren <basvansummeren@hotmail.com>
2015-10-06 14:51:49 +0200

    Some google sutffies!

---
+++ spring-ws/Calculator/src/main/java/nl/dare2date/matching/interests/SocialMediaConnection/YouTubeConnector.java
@@ -0,0 +1,24 @@
+package nl.dare2date.matching.interests.SocialMediaConnection;
+
+import nl.dare2date.matching.interests.Interest;
+
+import java.util.List;
+
+/**
+ * Created by Bas on 6-10-2015.
+ */
+public class YouTubeConnector implements SocialMediaConnector{
+
+    private static final String GOOGLE_ID ="423827042740-6o5c3tue58ta7m7ovf93cglg3kcnnhce.apps.googleusercontent.com";
+    private static final String GOOGLE_SECRET ="oTMoXqqsqk3L9Py6EU9t7ieR";
+
+    @Override
+    public StatusMessage validate(String smPassword, String smUserName) {
+        return null;
+    }
+
+    @Override
+    public List<Interest> getInterests() {
+        return null;
+    }
+}
