da0ff340fd01fe2ef5f2b0614c0f0cf2932daa65
Bas van Summeren <basvansummeren@hotmail.com>
2015-10-22 14:06:46 +0200

    The orchestration matchpoint is now succesfull!

--- spring-ws-jms/src/test/java/nl/dare2date/matching/orchestration/EndpointTest.java
+++ spring-ws-jms/src/test/java/nl/dare2date/matching/orchestration/EndpointTest.java
@@ -5,12 +5,11 @@ import nl.dare2date.matching.matching.Matcher;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
+import javax.jms.*;
 import javax.naming.NamingException;
 
 
@@ -22,6 +21,11 @@ public class EndpointTest {
     MatcherEndpoint endpoint;
     Matcher matcher;
     ConnectionFactory factory;
+    Connection conn;
+    Session ses;
+    MessageProducer producer;
+    MessageConsumer consumer;
+
     /**
      * Prepare some steps before
      */
@@ -30,15 +34,25 @@ public class EndpointTest {
         //Mock the matcher so that it does nothing
         matcher = Mockito.mock(Matcher.class);
         factory = Mockito.mock(ConnectionFactory.class);
+        conn=Mockito.mock(Connection.class);
+        ses = Mockito.mock(Session.class);
+        Mockito.when(factory.createConnection()).thenReturn(conn);
+        Mockito.when(conn.createSession(Matchers.anyBoolean(), Matchers.anyInt())).thenReturn(ses);
+
+        producer = Mockito.mock(MessageProducer.class);
+        Mockito.when(ses.createProducer((Destination) Matchers.any())).thenReturn(producer);
+        consumer = Mockito.mock(MessageConsumer.class);
+        Mockito.when(ses.createConsumer((Destination)Matchers.any())).thenReturn(consumer);
         //Create the endpoint with the mocks
         endpoint = new MatcherEndpoint(matcher, factory);
+
     }
 
     /**
      * test that the connectSocialMedia function gets delegated properly
      */
-    //@Test
-    public void testConnectSocialMediaConnect(){
+    @Test
+    public void testConnectSocialMediaConnect() throws JMSException {
         //Set up basic values for the test
         long userId = 5;
         String password = "gfdsgfsdgfsdg435435sdfgse46";
@@ -52,15 +66,22 @@ public class EndpointTest {
         MessageState state = MessageState.DATABASE_ERROR;
         String message = "Failure";
         //SPy at the statusmessage so that we can check if functions are called
-        nl.dare2date.matching.interests.socialMediaConnection.StatusMessage status = Mockito.spy(new nl.dare2date.matching.interests.socialMediaConnection.StatusMessage(state, message));
-        //Make Mockito return status when the connectsocialmedia is called
-        //Mockito.when(interestManager.connectSocialMedia(Matchers.anyLong(), (nl.dare2date.matching.interests.socialMediaConnection.SocialMediaType) Matchers.any(), Matchers.anyString())).thenReturn(status);
+        StatusMessage status = new StatusMessage();
+        status.setMessage(message);
+        status.setState(state);
+        Mockito.when(consumer.receive()).thenReturn(new BasicObjectMessage(status));
+
+
         //Do the fucntion we want to test
         ConnectSocialMediaResponse response = endpoint.connectSocialMedia(request);
         // Verify the interaction with out interestmanager mock
         //Mockito.verify(interestManager).connectSocialMedia(Matchers.eq(userId), Matchers.eq(nl.dare2date.matching.interests.socialMediaConnection.SocialMediaType.fromOrchestration(type)), Matchers.eq(password));
-        //Verfify the transformation
-        Mockito.verify(status).toOrchestration();
+
+        ArgumentCaptor<BasicObjectMessage> argument = ArgumentCaptor.forClass(BasicObjectMessage.class);
+        Mockito.verify(producer).send(argument.capture());
+        Assert.assertEquals(request, argument.getValue().getObject());
+        Assert.assertTrue(argument.getValue().getJMSCorrelationID().contains("User:5|For:FACE_BOOK|Time:"));
+
         //Check the response
         Assert.assertNotNull(response);
         Assert.assertNotNull(response.getResult());
